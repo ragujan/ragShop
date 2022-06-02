f12da558525abdd8e2366e0e85ae00eb86ad09fb
unknown <stiflerwedontgiveup.com>
2022-06-02 15:29:13 +0530

    jasperreport

--- src/TestingShit/JasperReportTest.java
+++ src/TestingShit/JasperReportTest.java
@@ -1,18 +1,48 @@
 package TestingShit;
 
-
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.HashMap;
+import net.sf.jasperreports.engine.JREmptyDataSource;
+import net.sf.jasperreports.engine.JasperCompileManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.JasperReport;
+import net.sf.jasperreports.view.JasperViewer;
 
 /*
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
-
 /**
  *
  * @author acer
  */
 public class JasperReportTest {
+
     public static void main(String[] args) {
-        
+        try {
+//            String fpath = "src//reports//Test.jasper";
+//            HashMap hm = new HashMap();
+//            hm.put("Parameter1","rag");
+//            Date date = new Date();
+//            SimpleDateFormat sdf = new SimpleDateFormat("hh:mm:ss a");
+//            
+//            hm.put("Parameter2", sdf.format(date));
+//            JREmptyDataSource jreds = new JREmptyDataSource();
+//            JasperPrint jp = JasperFillManager.fillReport(fpath, hm,jreds);
+//            JasperViewer.viewReport(jp);
+
+            String jspath = "src//reports//Test.jrxml";
+            JasperReport jr = JasperCompileManager.compileReport(jspath);
+            HashMap hm = new HashMap();
+            hm.put("Parameter1", "rag");
+            hm.put("Parameter2", "123");
+            
+            JREmptyDataSource jreds = new JREmptyDataSource();
+            JasperPrint jp = JasperFillManager.fillReport(jr, hm,jreds);
+            JasperViewer.viewReport(jp);
+        } catch (Exception e) {
+        }
     }
 }
